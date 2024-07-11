a0d27b4612a1efe96976ee92d596f17bc31fbbe3
Marcos Parreiras <marcosalp1998@gmail.com>
2024-07-11 11:54:06 -0300

    chore: prepare http server to deploy on render

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,8 @@
 import { env } from "../env";
 import { app } from "./app";
 
-app.listen({ port: env.PORT }).then(() => {
+const host = "RENDER" in process.env ? `0.0.0.0` : `localhost`;
+
+app.listen({ port: env.PORT, host }).then(() => {
   console.log(`Http server is running on port ${env.PORT}`);
 });
